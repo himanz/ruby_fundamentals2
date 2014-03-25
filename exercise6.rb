ee4227dbbd6157577c89ba4c1d600e4b3caf9c2f
Jonathan Chang <jonoschang7@gmail.com>
2014-03-25 14:12:33 -0400

    sort grocery list alphabetically and then output with asterisks

--- exercise6.rb
+++ exercise6.rb
@@ -15,11 +15,19 @@ list_items(grocery_list)
 puts "\n"
 
 puts "Items in grocery list to pick up is #{grocery_list.count}"
+puts "\n"
 
 if grocery_list.include?("bananas")
 	puts "You need to pick up bananas."
 else 
 	puts "You don't need to pick up bananas today."
 end
+puts "\n"
 
 puts grocery_list[1]
+puts "\n"
+
+grocery_list.sort!
+
+list_items(grocery_list)
+puts "\n"
